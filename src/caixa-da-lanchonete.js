13a9413b576425b151b37f7fa5e210f43adbd4b6
André Lopes <andrellopes021@gmail.com>
2023-08-14 11:24:47 -0300

    challenge completed

--- src/caixa-da-lanchonete.js
+++ src/caixa-da-lanchonete.js
@@ -2,42 +2,36 @@ const cardapio = [
   {
     codigo: "cafe",
     preco: 3.0,
-    descrição: "Café",
   },
   {
     codigo: "chantily",
     preco: 1.5,
-    descrição: "Chantily",
+    extraItemCode: "cafe",
   },
   {
     codigo: "suco",
     preco: 6.2,
-    descrição: "Suco Natural",
   },
   {
     codigo: "sanduiche",
     preco: 6.5,
-    descrição: "Sanduíche",
   },
   {
     codigo: "queijo",
     preco: 2.0,
-    descrição: "Queijo",
+    extraItemCode: "sanduiche",
   },
   {
     codigo: "salgado",
     preco: 7.25,
-    descrição: "Salgado",
   },
   {
     codigo: "combo1",
     preco: 9.5,
-    descrição: "1 Suco e 1 Sanduíche",
   },
   {
     codigo: "combo2",
     preco: 7.5,
-    descrição: "1 Café e 1 Sanduíche",
   },
 ];
 
@@ -45,48 +39,60 @@ class CaixaDaLanchonete {
 
   calcularValorDaCompra(metodoDePagamento, itens) {
     let valorTotal = 0;
-    let principalItems = [];
+    let mainItens = [];
+    let error = "";
 
-    if (!itens) {
-      return console.log("Não há itens no carrinho de compra!");
+    if (!itens.length) {
+      error = "Não há itens no carrinho de compra!";
     }
 
+    // Para cada item calcular o valor e fazer as validações => 
+
     itens.forEach((item) => {
+
+      // Separando o código e a quantidade de cada item do carrinho =>
       const [code, quantity] = item.split(",");
 
-      const itemCode = cardapio.filter((CItem) => CItem.codigo === code);
+      // Procurar o item no cardápido através do código =>
+      const CItem = cardapio.filter((item) => item.codigo === code);
 
-      if (Number(quantity) === 0) return console.log("Quantidade invalida!");
-      if (!itemCode.length) return console.log("Item inválido!");
+      // Validações de quantidade e código =>
+      if (Number(quantity) === 0) error = "Quantidade inválida!";
+      if (!CItem.length) error = "Item inválido!";
 
-      if (!["chantily", "queijo"].includes(code)) {
-        principalItems.push(code);
-      }
+      //Verificar se há item principal e somar os preços com a quantidade de cada item =>
+      CItem.forEach((CItem) => {
+        if (!CItem.extraItemCode) {
+          mainItens.push(code);
+        }
 
-      if (!principalItems.length) {
-        return console.log("Item extra não pode ser pedido sem o principal");
-      }
+        if (CItem.extraItemCode && !mainItens.includes(CItem.extraItemCode)) {
+          error = "Item extra não pode ser pedido sem o principal";
+        }
 
-      itemCode.forEach((CItem) => {
         valorTotal += CItem.preco * quantity;
       });
-
-      switch (metodoDePagamento) {
-        case "debito":
-          break;
-        case "credito":
-          valorTotal += valorTotal * 0.03;
-          break;
-        case "dinheiro":
-          valorTotal -= valorTotal * 0.05;
-          break;
-        default:
-          console.log("Forma de pagamento inválida!");
-          break;
-      }
     });
 
-    return console.log(`R$ ${valorTotal.toFixed(2).replace(".", ",")}`);
+    // Verificar e aplicar o desconto ou acréscimo de acordo com o método de pagamento =>
+    switch (metodoDePagamento) {
+      case "dinheiro":
+        valorTotal -= valorTotal * 0.05;
+        break;
+      case "credito":
+        valorTotal += valorTotal * 0.03;
+        break;
+      case "debito":
+        break;
+      default:
+        error = "Forma de pagamento inválida!";
+    }
+
+    // Se houver algum erro, encerrar a função e retornar o erro =>
+    if (error) return error;
+
+    // Se estiver tudo certo, retornar o valor fomatado com R$ 00,00 =>
+    return `R$ ${valorTotal.toFixed(2).replace(".", ",")}`;
   }
 }
 
